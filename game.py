ce74cadb90e619f6644ca298288edafd07188cd1
Eduardo <edu_7398@hotmail.com>
2019-01-17 10:17:03 -0500

    Modificacion: Mantener tecla presionada para movimiento continuo

--- game.py
+++ game.py
@@ -14,17 +14,28 @@ rect_ball = spr_ball.get_rect()
 
 color = pygame.Color(120,100,60)
 
+x,y = (50,30)
+
 #Game Loop
 while True:
-    x,y = pygame.mouse.get_pos()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        y -= 5
+    elif keys[pygame.K_DOWN]:
+        y += 5
+    elif keys[pygame.K_LEFT]:
+        x -= 5
+    elif keys[pygame.K_RIGHT]:
+        x += 5
+    #x,y = pygame.mouse.get_pos()
     #gestionar los eventos del usuario
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             print(event.button)
-        if event.type == pygame.QUIT:
+        elif event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.KEYDOWN:
+        """elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 rect_ball = rect_ball.move([10,0])
             elif event.key == pygame.K_LEFT:
@@ -32,7 +43,7 @@ while True:
             elif event.key == pygame.K_UP:
                 rect_ball = rect_ball.move([0,-10])
             elif event.key == pygame.K_DOWN:
-                rect_ball = rect_ball.move([0,10])
+                rect_ball = rect_ball.move([0,10])"""
     #actualizar los estados del juego
     """if velocidad[0] > 0:
         velocidad[0] = random.randint(1,5)
@@ -53,8 +64,8 @@ while True:
         velocidad[0] = random.randint(1,5)"""
     #renderizar la interfaz grafica
     screen.fill((0,0,0))
-    screen.blit(spr_ball,((x-spr_ball.get_width()/2),(y-spr_ball.get_height()/2)))
-    pygame.draw.line(screen,color,(60,60),(x,y))
-    pygame.draw.circle(screen,(102,120,150),(x,y),32,5)
+    screen.blit(spr_ball,(x,y))
+    #pygame.draw.line(screen,color,(60,60),(x,y))
+    #pygame.draw.circle(screen,(102,120,150),(x,y),32,5)
     pygame.display.flip()
 
